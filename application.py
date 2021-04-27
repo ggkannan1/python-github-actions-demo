95f207bfdf4e9271d2498b7b88b85216715664b1
Kannan <ggkannan1@gmail.com>
2021-04-27 15:01:32 +0000

    updated app.py

--- application.py
+++ application.py
@@ -4,7 +4,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World! Welcome to Devops!"
+    return "Hello World! Welcome to Devops 2021!"
 
 # run the app.
 if __name__ == "__main__":
