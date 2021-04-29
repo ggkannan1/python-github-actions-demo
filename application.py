6b1cc6207ee54d683c53305b515a218b307bf6b4
Kannan <ggkannan1@gmail.com>
2021-04-29 08:24:26 +0000

    update code

--- application.py
+++ application.py
@@ -4,7 +4,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World! Welcome to CI/CD"
+    return "Hello World! Welcome to CI/CD Deploy"
 
 # run the app.
 if __name__ == "__main__":
